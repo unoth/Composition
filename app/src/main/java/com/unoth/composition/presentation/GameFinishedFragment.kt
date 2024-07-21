94d4473a2524f483e144d8bb25f68e20fc288536
unoth <dan005bk@gmail.com>
2024-07-22 01:11:46 +0300

    feat(): add viewBinding in fragments

--- app/src/main/java/com/unoth/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/unoth/composition/presentation/GameFinishedFragment.kt
@@ -1,19 +1,27 @@
 package com.unoth.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.unoth.composition.R
+import androidx.fragment.app.Fragment
+import com.unoth.composition.databinding.FragmentGameFinishedBinding
 
 class GameFinishedFragment : Fragment() {
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
