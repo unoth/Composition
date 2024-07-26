011fe438e8a591706ca4aeaabe50fbe7ae951738
unoth <dan005bk@gmail.com>
2024-07-26 14:15:56 +0300

    feat(): add viewBinding in fragments

--- app/src/main/java/com/unoth/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/unoth/composition/presentation/GameFragment.kt
@@ -15,7 +15,7 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
