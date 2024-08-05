df0258df78e33a9ac0f58d72c9499b9dfee7a530
unoth <dan005bk@gmail.com>
2024-08-05 23:40:53 +0300

    feat(GameViewModel): add question generation and progress percent

--- app/src/main/java/com/unoth/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/unoth/composition/presentation/GameViewModel.kt
@@ -1,17 +1,23 @@
 package com.unoth.composition.presentation
 
 import android.annotation.SuppressLint
+import android.app.Application
 import android.os.CountDownTimer
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.unoth.composition.R
 import com.unoth.composition.data.GameRepositoryImpl
+import com.unoth.composition.domain.entity.GameResult
 import com.unoth.composition.domain.entity.GameSettings
 import com.unoth.composition.domain.entity.Level
+import com.unoth.composition.domain.entity.Question
 import com.unoth.composition.domain.usecase.GenerateQuestionUseCase
 import com.unoth.composition.domain.usecase.GetGameSettingsUseCase
 
-class GameViewModel : ViewModel() {
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val context = application
 
     private lateinit var level: Level
     private lateinit var gameSettings: GameSettings
@@ -22,18 +28,63 @@ class GameViewModel : ViewModel() {
 
     private var timer: CountDownTimer? = null
 
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
+
     private val _formattedTime = MutableLiveData<String>()
     val formattedTime: LiveData<String>
         get() = _formattedTime
 
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = _question
+
+    private val _percentOfRightAnswer = MutableLiveData<Int>()
+    val percentOfRightAnswer: LiveData<Int>
+        get() = _percentOfRightAnswer
+
+    private val _progressAnswers = MutableLiveData<String>()
+    val progressAnswers: LiveData<String>
+        get() = _progressAnswers
+
+
+    private val _enoughRightAnswers = MutableLiveData<Boolean>()
+    val enoughRightAnswers: LiveData<Boolean>
+        get() = _enoughRightAnswers
+
+    private val _enoughPercentAnswers = MutableLiveData<Boolean>()
+    val enoughPercentAnswers: LiveData<Boolean>
+        get() = _enoughPercentAnswers
+
+    private val _minPercent = MutableLiveData<Int>()
+    val minPercent: LiveData<Int>
+        get() = _minPercent
+
+    private val _gameResult = MutableLiveData<GameResult>()
+    val gameResult: LiveData<GameResult>
+        get() = _gameResult
+
+
     fun startGame(level: Level) {
         getGameSetting(level)
+        generateQuestion()
         startTimer()
+    }
+
+    fun choiceAnswer(number: Int) {
+        val rightAnswer = question.value?.rightAnswer
+        if (number == rightAnswer) {
+            countOfRightAnswers++
+        }
+        countOfQuestions++
+        updateProgress()
+        generateQuestion()
     }
 
     private fun getGameSetting(level: Level) {
         this.level = level
         this.gameSettings = getGameSettingsUseCase(level)
+        _minPercent.value = gameSettings.minPercentOfRightAnswer
     }
 
     private fun startTimer() {
@@ -48,7 +99,6 @@ class GameViewModel : ViewModel() {
             override fun onFinish() {
                 finishGame()
             }
-
         }
         timer?.start()
     }
@@ -62,8 +112,36 @@ class GameViewModel : ViewModel() {
         return String.format("%02d:%02d", min, leftSec)
     }
 
+    private fun generateQuestion() {
+        _question.value = generateQuestionUseCase(gameSettings.maxSumValue)
+    }
+
+    @SuppressLint("StringFormatMatches")
+    private fun updateProgress() {
+        val percentProgress = percentOfProgress()
+        _percentOfRightAnswer.value = percentProgress
+        _progressAnswers.value = String.format(
+            context.resources.getString(R.string.progress_answers),
+            countOfRightAnswers,
+            gameSettings.minCountOfRightAnswers
+        )
+        _enoughRightAnswers.value =
+            countOfRightAnswers >= gameSettings.minCountOfRightAnswers //min for true
+        _enoughPercentAnswers.value =
+            percentProgress >= gameSettings.minPercentOfRightAnswer //min for true
+    }
+
+    private fun percentOfProgress(): Int {
+        return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+
     private fun finishGame() {
-        //Implementation
+        _gameResult.value = GameResult(
+            enoughRightAnswers.value == true && enoughPercentAnswers.value == true,
+            countOfRightAnswers,
+            countOfQuestions,
+            gameSettings
+        )
     }
 
     override fun onCleared() {
