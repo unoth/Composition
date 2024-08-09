4a131e5a16f393e92f416fca2e869343942350d0
unoth <dan005bk@gmail.com>
2024-08-09 20:22:12 +0300

    feat(GameFragment): implementation of GameFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.unoth.composition"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
